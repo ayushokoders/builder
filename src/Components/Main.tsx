1ec027b6b0f99bc2ce972ff8299a4931a4927c86
ayushokoders <108781318+ayushokoders@users.noreply.github.com>
2022-07-14 13:37:21 +0530

    Update Main.tsx

--- src/Components/Main.tsx
+++ src/Components/Main.tsx
@@ -1,14 +1,26 @@
-import { useEffect } from "react";
+import { CopyAll } from "@mui/icons-material";
+import { Stack, Typography, Button, Divider, IconButton } from "@mui/material";
+import MockData from "../Mock/MockData";
+import React from "react";
 
 function Main({content} : {content : string}){
 
-    useEffect(()=>{
-        console.log(content)
-    },[content])
     return(
-        <>
-
-        </>
+        <Stack gap={4} m={10} sx={{width:"60%"}}>
+            <Typography variant="h4" textAlign="left">{MockData[0].title}</Typography>
+            <Typography variant="h6" textAlign="left">{MockData[0].description}</Typography>
+            <Stack height="250px" width="400px" direction="row" alignItems="center" justifyContent="center" gap={7} border="1px solid gray" p={4} sx={{position:"relative"}}>
+                <Button sx={{position:"absolute", top:"2%", left:".5%"}} variant="text">
+                    <img src="" alt="image" loading="lazy" />
+                </Button>
+                <Typography variant="body2">{ MockData[0].code }</Typography>
+                <IconButton sx={{position:"absolute", top:"2%", right:".5%"}} 
+                    color="primary" aria-label="upload picture" component="span" 
+                    onClick={()=>navigator.clipboard.writeText(MockData[0].code)}>
+                    <CopyAll/>
+                </IconButton>
+            </Stack>
+        </Stack>
     )
 }
 
